a2e77563359d555eb067c5cab7ec46c5ce73420b
kapitanluffy <485799+kapitanluffy@users.noreply.github.com>
2025-07-10 16:52:59 +0800

    Rework expand and collapse sidebar folders

--- src/collapse_folder.py
+++ src/collapse_folder.py
@@ -1,3 +1,4 @@
+import os
 import sublime
 import sublime_plugin
 from .utils import is_setting_enabled, collapse_folder, get_expanded_folders, get_project_data
@@ -11,10 +12,16 @@ class TweaksCollapseFolderCommand(sublime_plugin.WindowCommand):
         project_data = get_project_data(self.window)
         folders = []
 
+        window_variables = self.window.extract_variables()
+        project_path = window_variables["project_path"]
+
         for i,v in enumerate(expanded_folders):
             for f in project_data['folders']:
-                if f['path'] == v:
-                    folders.append(v)
+                project_folder_path = f['path']
+                if not os.path.isabs(project_folder_path):
+                    project_folder_path = os.path.abspath(os.path.join(project_path, project_folder_path))
+                if project_folder_path == v:
+                    folders.append(f['path'])
 
         self.window.show_quick_panel(folders, lambda index: self.on_done(index, folders))
 
